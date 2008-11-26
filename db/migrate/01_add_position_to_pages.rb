a3ad82c5678aee6c63823f71ebe88bdb9b71cad2
Chris Parrish <chris.parrish-gitmail@swankinnovations.com>
2008-11-25 20:32:14 -0700

    Disable Rails timestamps while adding position so updated_at isn't altered

--- db/migrate/01_add_position_to_pages.rb
+++ db/migrate/01_add_position_to_pages.rb
@@ -3,9 +3,11 @@ class AddPositionToPages < ActiveRecord::Migration
     add_column :pages, :position, :integer
     Page.reset_column_information
     say_with_time("Putting all pages in a default order...") do
+      ActiveRecord::Base.record_timestamps = false
       Page.find_all_by_parent_id(nil).each do |p|
         put_children_into_list(p)
       end
+      ActiveRecord::Base.record_timestamps = true
     end
   end
   
