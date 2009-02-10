3bf8a97adb7d831a123c997044d94866ca8e1aed
Dayne Broderson <dayne@gina.alaska.edu>
2009-02-10 10:55:26 +0800

    fix Admin::PagesController map route for whole extension
    Signed-off-by: Radiant CMS dev team <radiant@prime-motif.com>

--- reorder_extension.rb
+++ reorder_extension.rb
@@ -2,12 +2,12 @@
 require_dependency 'application'
 
 class ReorderExtension < Radiant::Extension
-  version "0.1"
+  version "0.1.1"
   description "Allows (re)ordering of pages in the page tree."
   url "http://dev.radiantcms.org/"
   
   define_routes do |map|
-    map.with_options :controller => "admin/page" do |page|
+    map.with_options :controller => "admin/pages" do |page|
       page.page_move_lower "admin/pages/:id/move_lower", :action => "move_lower"
       page.page_move_higher "admin/pages/:id/move_higher", :action => "move_higher"
       page.page_move_to_bottom "admin/pages/:id/move_to_bottom", :action => "move_to_bottom"
